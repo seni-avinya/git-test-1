ff427ac4037e33ba18821088a9ed2654836303b0
seni-avinya <senipatel.avinya@gmail.com>
2025-12-01 10:28:54 +0530

    new code from vs code of branch-1

---
+++ lib/branch1Test1.dart
@@ -0,0 +1,3 @@
+// this is new code from branch -1 and push to the master branch 
+// i am using VS code for that.
+// first directly add into mater and then fetch data from master
--- lib/main.dart
+++ lib/main.dart
@@ -64,3 +64,9 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+// this is new code from branch -1 and push to the master branch 
+// i am using VS code for that.
+// first directly add into mater and then fetch data from master
+
+
